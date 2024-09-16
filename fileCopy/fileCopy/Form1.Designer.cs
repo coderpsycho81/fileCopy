5db46aaa7a67ca552fe68a1afe24513216a14d14
coderpsycho <101251156+coderpsycho81@users.noreply.github.com>
2024-09-16 14:37:55 +0300

    Add files via upload

---
+++ fileCopy/fileCopy/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace fileCopy
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDestination = new System.Windows.Forms.TextBox();
+            this.btnSource = new System.Windows.Forms.Button();
+            this.btnDestination = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.labelProgress = new System.Windows.Forms.Label();
+            this.labelSizeCopied = new System.Windows.Forms.Label();
+            this.labelTimeLeft = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kaynak: ";
+            // 
+            // txtSource
+            // 
+            this.txtSource.Location = new System.Drawing.Point(104, 12);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(206, 20);
+            this.txtSource.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(12, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Hedef: ";
+            // 
+            // txtDestination
+            // 
+            this.txtDestination.Location = new System.Drawing.Point(104, 42);
+            this.txtDestination.Name = "txtDestination";
+            this.txtDestination.Size = new System.Drawing.Size(206, 20);
+            this.txtDestination.TabIndex = 1;
+            // 
+            // btnSource
+            // 
+            this.btnSource.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSource.Location = new System.Drawing.Point(316, 11);
+            this.btnSource.Name = "btnSource";
+            this.btnSource.Size = new System.Drawing.Size(63, 23);
+            this.btnSource.TabIndex = 2;
+            this.btnSource.Text = "...";
+            this.btnSource.UseVisualStyleBackColor = true;
+            this.btnSource.Click += new System.EventHandler(this.btnSource_Click);
+            // 
+            // btnDestination
+            // 
+            this.btnDestination.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDestination.Location = new System.Drawing.Point(316, 40);
+            this.btnDestination.Name = "btnDestination";
+            this.btnDestination.Size = new System.Drawing.Size(63, 23);
+            this.btnDestination.TabIndex = 2;
+            this.btnDestination.Text = "...";
+            this.btnDestination.UseVisualStyleBackColor = true;
+            this.btnDestination.Click += new System.EventHandler(this.btnDestination_Click);
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCopy.Location = new System.Drawing.Point(12, 72);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(367, 31);
+            this.btnCopy.TabIndex = 3;
+            this.btnCopy.Text = "Kopyala";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(73, 109);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(306, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // labelProgress
+            // 
+            this.labelProgress.AutoSize = true;
+            this.labelProgress.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelProgress.Location = new System.Drawing.Point(9, 112);
+            this.labelProgress.Name = "labelProgress";
+            this.labelProgress.Size = new System.Drawing.Size(63, 16);
+            this.labelProgress.TabIndex = 5;
+            this.labelProgress.Text = "İlerleme";
+            // 
+            // labelSizeCopied
+            // 
+            this.labelSizeCopied.AutoSize = true;
+            this.labelSizeCopied.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelSizeCopied.Location = new System.Drawing.Point(9, 145);
+            this.labelSizeCopied.Name = "labelSizeCopied";
+            this.labelSizeCopied.Size = new System.Drawing.Size(186, 16);
+            this.labelSizeCopied.TabIndex = 7;
+            this.labelSizeCopied.Text = "Kopyalanan dosya boyutu";
+            // 
+            // labelTimeLeft
+            // 
+            this.labelTimeLeft.AutoSize = true;
+            this.labelTimeLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelTimeLeft.Location = new System.Drawing.Point(9, 170);
+            this.labelTimeLeft.Name = "labelTimeLeft";
+            this.labelTimeLeft.Size = new System.Drawing.Size(84, 16);
+            this.labelTimeLeft.TabIndex = 8;
+            this.labelTimeLeft.Text = "Kalan süre:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 190);
+            this.Controls.Add(this.labelTimeLeft);
+            this.Controls.Add(this.labelSizeCopied);
+            this.Controls.Add(this.labelProgress);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.btnDestination);
+            this.Controls.Add(this.btnSource);
+            this.Controls.Add(this.txtDestination);
+            this.Controls.Add(this.txtSource);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(400, 229);
+            this.MinimumSize = new System.Drawing.Size(400, 229);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "File Copy / CODERPSYCHO";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDestination;
+        private System.Windows.Forms.Button btnSource;
+        private System.Windows.Forms.Button btnDestination;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label labelProgress;
+        private System.Windows.Forms.Label labelSizeCopied;
+        private System.Windows.Forms.Label labelTimeLeft;
+    }
+}
+
---
+++ fileCopy/fileCopy/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace fileCopy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int totalFiles = 0;
+        private int copiedFiles = 0;
+        private long totalBytes = 0;
+        private long copiedBytes = 0;
+        private Stopwatch stopwatch = new Stopwatch();
+
+        private void btnSource_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+            if (folderDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtSource.Text = folderDialog.SelectedPath;
+            }
+        }
+
+        private void btnDestination_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+            if (folderDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtDestination.Text = folderDialog.SelectedPath;
+            }
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            string sourceDir = txtSource.Text;
+            string destinationDir = txtDestination.Text;
+
+            progressBar1.Value = 0;
+            labelProgress.Text = "0%";
+            labelSizeCopied.Text = "Aktarılan: 0 GB";
+            labelTimeLeft.Text = "Kalan süre: Hesaplanıyor...";
+
+            try
+            {
+                totalFiles = CountFilesAndDirectories(sourceDir);
+                totalBytes = CalculateTotalSize(sourceDir);
+                progressBar1.Maximum = totalFiles;
+
+                stopwatch.Restart();
+
+                CopyDirectory(sourceDir, destinationDir);
+
+                stopwatch.Stop();
+                MessageBox.Show("Kopyalama işlemi tamamlandı!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata: " + ex.Message);
+            }
+        }
+
+        private void CopyDirectory(string sourceDir, string destinationDir)
+        {
+            Directory.CreateDirectory(destinationDir);
+
+            foreach (var file in Directory.GetFiles(sourceDir))
+            {
+                string destFile = Path.Combine(destinationDir, Path.GetFileName(file));
+                FileInfo fileInfo = new FileInfo(file);
+
+                File.Copy(file, destFile, true);
+                copiedBytes += fileInfo.Length;
+
+                UpdateProgress(fileInfo.Length);
+            }
+
+            foreach (var dir in Directory.GetDirectories(sourceDir))
+            {
+                string destDir = Path.Combine(destinationDir, Path.GetFileName(dir));
+                CopyDirectory(dir, destDir);
+            }
+        }
+
+        private int CountFilesAndDirectories(string dir)
+        {
+            int count = Directory.GetFiles(dir).Length;
+            foreach (var subDir in Directory.GetDirectories(dir))
+            {
+                count += CountFilesAndDirectories(subDir);
+            }
+            return count;
+        }
+
+        private long CalculateTotalSize(string dir)
+        {
+            long size = 0;
+            foreach (var file in Directory.GetFiles(dir))
+            {
+                FileInfo fileInfo = new FileInfo(file);
+                size += fileInfo.Length;
+            }
+            foreach (var subDir in Directory.GetDirectories(dir))
+            {
+                size += CalculateTotalSize(subDir);
+            }
+            return size;
+        }
+
+        private void UpdateProgress(long fileSize)
+        {
+            copiedFiles++;
+            progressBar1.Value = copiedFiles;
+
+            int percentage = (int)((double)copiedFiles / totalFiles * 100);
+            labelProgress.Text = percentage.ToString() + "%";
+
+            double copiedGB = (double)copiedBytes / (1024 * 1024 * 1024); 
+            labelSizeCopied.Text = $"Aktarılan: {copiedGB:F2} GB";
+
+            double elapsedSeconds = stopwatch.Elapsed.TotalSeconds;
+            double bytesPerSecond = copiedBytes / elapsedSeconds;
+            double remainingBytes = totalBytes - copiedBytes;
+            double estimatedTimeLeft = remainingBytes / bytesPerSecond; 
+            TimeSpan timeLeft = TimeSpan.FromSeconds(estimatedTimeLeft);
+            labelTimeLeft.Text = $"Kalan süre: {timeLeft.Minutes} dk {timeLeft.Seconds} sn";
+
+            Application.DoEvents();
+        }
+    }
+}
